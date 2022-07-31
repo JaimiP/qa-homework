bf982c753ee83f17537f6e4145a8aab4bc00ed25
JaimiP <mazerbear@gmail.com>
2022-07-30 22:37:53 -0600

    Moving basic auth test to a separate test file.

---
+++ test/basic_auth_test.exs
@@ -0,0 +1,16 @@
+defmodule HomeworkTest do
+  # Import helpers
+  use Hound.Helpers
+  use ExUnit.Case
+
+  # Start hound session and destroy when tests are run
+  hound_session()
+
+  test "basic auth test" do
+    # Pass in authentication info in URL.
+    # This could also be done by assigning and referencing variables.
+    navigate_to "https://admin:admin@the-internet.herokuapp.com/basic_auth"
+    # =~ matches the term on the left against the string on the right.
+    assert visible_page_text() =~ "Congratulations!"
+  end
+end
--- test/homework_test.exs
+++ test/homework_test.exs
@@ -9,12 +9,4 @@ defmodule HomeworkTest do
   test "goes to google" do
     navigate_to "http://google.com"
   end
-
-  test "basic auth test" do
-    # Pass in authentication info in URL.
-    # This could also be done by assigning and referencing variables.
-    navigate_to "https://admin:admin@the-internet.herokuapp.com/basic_auth"
-    # =~ matches the term on the left against the string on the right.
-    assert visible_page_text() =~ "Congratulations!"
-  end
 end
