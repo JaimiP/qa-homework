ebedadeabed1888ff1ad1428539d980a77dcac7c
JaimiP <mazerbear@gmail.com>
2022-07-30 21:21:36 -0600

    Second iteration of basic auth test completed

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -15,7 +15,6 @@ defmodule HomeworkTest do
     # This could also be done by assigning and referencing variables.
     navigate_to "https://admin:admin@the-internet.herokuapp.com/basic_auth"
     # =~ matches the term on the left against the string on the right.
-    assert current_url() =~ "basic_auth"
-    #TODO: Fix assertion. This isn't ideal, but it's a start while I figure out the new tech stack
+    assert visible_page_text() =~ "Congratulations!"
   end
 end
