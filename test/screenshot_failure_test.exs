866fe9d3cc1a80267ddd78ae9a3b5d710b23a1da
JaimiP <mazerbear@gmail.com>
2022-07-31 23:39:55 -0600

    Implemented screenshot on failure. Tried to add timestamp to screenshot name with screen interpolation but ended up removing.

--- test/screenshot_failure_test.exs
+++ test/screenshot_failure_test.exs
@@ -6,14 +6,22 @@ defmodule ScreenshotFailureTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "failing login test" do
-    navigate_to "https://the-internet.herokuapp.com/login"
-    # Locate fields and enter login credentials.
-    fill_field({:id, "username"}, "Divvy's next top Employee: Jaimi!")
-    fill_field({:id, "password"}, "SuperSecretPassword!")
-    # Submit form.
-    submit_element({:css, "[type='submit']"})
-    # Check for login failure.
-    assert element_displayed?({:class, "success"})
+  test "failing screenshot test" do
+    try do
+      navigate_to "https://the-internet.herokuapp.com/login"
+      # Locate fields and enter login credentials.
+      fill_field({:id, "username"}, "Divvy's next top Employee: Jaimi!")
+      fill_field({:id, "password"}, "SuperSecretPassword!")
+      # Submit form.
+      submit_element({:css, "[type='submit']"})
+      # Check for login failure.
+      assert element_displayed?({:class, "success"})I
+    rescue
+      error ->
+        test_name = "screenshot test"
+        # I'd love to chat about this section.
+        take_screenshot("./screenshots/#{test_name}.png")
+        raise error
+    end
   end
 end
