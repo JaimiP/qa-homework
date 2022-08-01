bcaf123bdadf42264d4317544e5ab03bc84ef5a2
JaimiP <mazerbear@gmail.com>
2022-07-31 23:49:05 -0600

    Cleaned up comments in prep to submit homework.

--- test/basic_auth_test.exs
+++ test/basic_auth_test.exs
@@ -9,14 +9,14 @@ defmodule AuthTest do
   test "basic auth test" do
     # Pass in authentication info in URL.
     navigate_to "https://admin:admin@the-internet.herokuapp.com/basic_auth"
-    # =~ matches the term on the left against the string on the right.
+    # Confirm success message is visible.
     assert visible_page_text() =~ "Congratulations!"
   end
 
   test "basic auth invalid credentials test" do
-    # Pass in authentication info in URL.
+    # Pass in invalid authentication info in URL.
     navigate_to "https://ADMIN:pass@the-internet.herokuapp.com/basic_auth"
-    # =~ matches the term on the left against the string on the right.
+    # Confirm success message is missing.
     refute visible_page_text() =~ "Congratulations!"
   end
 end
--- test/screenshot_failure_test.exs
+++ test/screenshot_failure_test.exs
@@ -15,11 +15,13 @@ defmodule ScreenshotFailureTest do
       # Submit form.
       submit_element({:css, "[type='submit']"})
       # Check for login failure.
-      assert element_displayed?({:class, "success"})I
+      assert element_displayed?({:class, "success"})
     rescue
       error ->
         test_name = "screenshot test"
         # I'd love to chat about this section.
+        # I tried to implement a timestamp in the screenshot title so that locating a specific failed test image would be easy.
+        # I ran out of time trying to do so.
         take_screenshot("./screenshots/#{test_name}.png")
         raise error
     end
