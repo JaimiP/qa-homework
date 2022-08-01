32934d6ce27bdc4094cad1b772b3f06f2b85e895
JaimiP <mazerbear@gmail.com>
2022-07-31 21:08:02 -0600

    Added additional tests to form_auth_test file.

--- test/form_auth_test.exs
+++ test/form_auth_test.exs
@@ -18,7 +18,7 @@ defmodule FormAuthTest do
     submit_element({:css, "[type='submit']"})
     # Check for success alert presence.
     assert element_displayed?({:class, "success"})
-    # Could also be verified with URL change, presence of logout button, or successful API response.
+    # Could be verified a few ways. Presence of logout button or successful API response for example.
   end
 
   test "incorrect password login test" do
@@ -31,7 +31,18 @@ defmodule FormAuthTest do
     send_text("AWrongPassword123")
     # Submit form.
     submit_element({:css, "[type='submit']"})
-    # Check for invalid password alert presence. Deliberately choosing a different verification method than the successful test.
+    # Check for login failure. Deliberately choosing a different verification method than the successful test.
     assert visible_page_text() =~ "password is invalid"
+  end
+
+  test "incorrect username login test" do
+    navigate_to "https://the-internet.herokuapp.com/login"
+    # Locate fields and enter login info (Different than previous tests)
+    fill_field({:id, "username"}, "Divvy's next top Employee: Jaimi!")
+    fill_field({:id, "password"}, "SuperSecretPassword!")
+    # Submit form.
+    submit_element({:css, "[type='submit']"})
+    # Check for login failure. Again, this could be done several ways.
+    assert visible_page_text() =~ "username is invalid"
   end
 end
