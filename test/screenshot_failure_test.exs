f9ff857e01fa5c35a6845cc6c532001b80045175
JaimiP <mazerbear@gmail.com>
2022-08-01 21:45:16 -0600

    First draft, messy but added date and time in title of screenshots from screenshot on failure test.

--- test/screenshot_failure_test.exs
+++ test/screenshot_failure_test.exs
@@ -19,9 +19,52 @@ defmodule ScreenshotFailureTest do
     rescue
       error ->
         test_name = "screenshot test"
-        # I'd love to chat about this section.
-        # I tried to implement a timestamp in the screenshot title so that locating a specific failed test image would be easy.
-        # I ran out of time trying to do so.
+        defmodule ScreenshotFailureTest do
+          # Import helpers
+          use Hound.Helpers
+          use ExUnit.Case
+          #use Timex
+
+          # Start hound session and destroy when tests are run
+          hound_session()
+
+          test "failing screenshot test" do
+            try do
+              navigate_to "https://the-internet.herokuapp.com/login"
+              # Locate fields and enter login credentials.
+              fill_field({:id, "username"}, "Divvy's next top Employee: Jaimi!")
+              fill_field({:id, "password"}, "SuperSecretPassword!")
+              # Submit form.
+              submit_element({:css, "[type='submit']"})
+              # Check for login failure.
+              assert element_displayed?({:class, "success"})
+            rescue
+              error ->
+                test_name = "screenshot test"
+                # date_time = Timex.now()
+                dt_now = DateTime.utc_now
+                # Previous try at using strftime which I've used before.
+                #test_time = Timex.parse!(date_time,"%d.%m.%Y %H %M %Z",:strftime)
+                #truncate date time to remove miliseconds
+                tdt = DateTime.truncate(dt_now, :second)
+                # change type to string
+                tdt_string = DateTime.to_string(tdt)
+                # remove Z from end of string and replace with default time zone.
+                tdt_noz = String.replace(tdt_string, "Z", " utc")
+                # remove : and replace with . so that file name is possible.
+                tdt_clean = String.replace(tdt_noz, ":", ".")
+                # Trial and error puts for debugging
+                IO.puts dt_now
+                IO.puts tdt
+                IO.puts tdt_string
+                IO.puts tdt_noz
+                IO.puts tdt_clean
+                take_screenshot("./screenshots/#{test_name} #{tdt_clean}.png")
+                raise error
+            end
+          end
+        end
+
         take_screenshot("./screenshots/#{test_name}.png")
         raise error
     end
