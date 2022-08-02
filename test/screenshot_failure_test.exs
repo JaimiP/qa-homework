8e5f8473d9d09a1a343a67b65e183b4e77938d7f
JaimiP <mazerbear@gmail.com>
2022-08-01 21:55:11 -0600

    Added a second failing test to demonstrate test name in screenshot title.

--- test/screenshot_failure_test.exs
+++ test/screenshot_failure_test.exs
@@ -6,7 +6,7 @@ defmodule ScreenshotFailureTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "failing screenshot test" do
+  test "failing login screenshot test" do
     try do
       navigate_to "https://the-internet.herokuapp.com/login"
       # Locate fields and enter login credentials.
@@ -18,7 +18,35 @@ defmodule ScreenshotFailureTest do
       assert element_displayed?({:class, "success"})
     rescue
       error ->
-        test_name = "screenshot test"
+        test_name = "login image test"
+        dt_now = DateTime.utc_now
+        #truncate date time to remove miliseconds
+        trunc_dt = DateTime.truncate(dt_now, :second)
+        # change type to string
+        tdt_string = DateTime.to_string(trunc_dt)
+        # remove Z from end of string and replace with default time zone.
+        tdt_noz = String.replace(tdt_string, "Z", " utc")
+        # remove : and replace with . so that file name is possible.
+        tdt_clean = String.replace(tdt_noz, ":", ".")
+        take_screenshot("./screenshots/#{test_name} #{tdt_clean}.png")
+        raise error
+    end
+  end
+
+  test "" do
+    try do
+      navigate_to "https://the-internet.herokuapp.com/dropdown"
+      # Check that dropdown is displayed.
+      element_displayed?({:id, "dropdown"})
+      # Click on dropdown
+      click({:id, "dropdown"})
+      # Click desired option, located using xpath.
+      click({:xpath, "//div/select/option[contains(text(), 'Option 1')]"})
+      # Make sure the visible text in the drop down matches the selected option.
+      assert visible_text({:id, "dropdown"}) =~ "Forcing failure"
+    rescue
+      error ->
+        test_name = "dropdown image test"
         dt_now = DateTime.utc_now
         #truncate date time to remove miliseconds
         trunc_dt = DateTime.truncate(dt_now, :second)
